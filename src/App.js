6a383cca9ef0d1b4868570430ebb2ca06059b1f9
Lordcreos <leonardosanchez4h@hotmail.com>
2021-07-31 01:20:15 -0500

    cliente react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,115 @@
-import logo from './logo.svg';
+
 import './App.css';
+import MaterialTable from 'material-table';
+import { React, useEffect, useState,  } from 'react';
+import {tableIcons} from './components/icons.js';
+import axios from 'axios';
+
+
+const api = 'http://localhost:5000'
+
+
+
+
+
+const columns = [
+  {    title: 'Name',  field: 'name'},
+  {    title: 'Age',    field: 'age',    type: "numeric"   },
+  {    title: 'First Day',    field: 'meetDay',    type: "date"    }  ,
+  {    title: 'Address',    field: 'address'   },
+  {    title: 'Number',    field: 'number'   }, 
+  {  title: 'Email',    field: 'email'   }
+]
 
 function App() {
+const [data,setData] = useState([]);
+
+
+
+
+ useEffect(() => {
+ 
+    getContact();
+    
+ },[]);
+
+ const getContact = async () => {
+  await axios.get(api).then(response =>{
+    setData(response.data);
+    
+  })
+}  
+
+ const create = async (newData)=>{
+   try {
+     await axios.post(api, newData);
+    console.log(newData);
+      
+   } catch (error) {
+     console.log(error);
+   }
+   
+ }
+
+ const deleted = async (deleteData)=>{
+   try {
+    axios.delete(`${api}/${deleteData._id}`)
+     console.log(deleteData._id);
+     
+   } catch (error) {
+     console.log (error);
+   }
+ } 
+
+ const update = async (updatedData) =>{
+   try {
+      axios.patch(`${api}/${updatedData._id}`, updatedData);
+      console.log(updatedData._id);
+   } catch (error) {
+     console.log(error)
+   }
+
+ }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MaterialTable
+      columns = {columns}
+      data= {data}
+      title= "covid contact app"
+      icons={tableIcons}
+      localization={{ header:{  actions: "Acciones"    }, body: { editRow: { deleteText: '¿Estas seguro de querer eliminar?' }} }}
+      editable =  {{
+        onRowAdd : (newRow)=>  new Promise((resolve,reject)=>{
+          const newData =  newRow
+           create(newData);
+           window.location.reload();
+           resolve();
+        }),
+        onRowDelete: (selectedRow) => new Promise ((resolve,reject )=>{
+          const deleteData = selectedRow;
+          deleted(deleteData);
+          
+          window.location.reload();
+          resolve();
+        }),
+        onRowUpdate: (updatedRow, oldrow )=> new Promise((reject,resolve)=>{
+          const updatedData = updatedRow;
+          const oldData= oldrow;
+          update(updatedData);
+          window.location.reload();
+          resolve();
+        })
+      }}
+
+      options={{
+        actionsColumnIndex: -1 
+      }}
+
+    />
+    
   );
 }
 
